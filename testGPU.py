128a48745b41896c8464bfafbc3ee914545879e0
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-09-25 14:12:03 +0000

    Add files via upload

---
+++ testGPU.py
@@ -0,0 +1,19 @@
+import torch
+
+try:
+    deviceName = torch.cuda.get_device_name(torch.cuda.current_device())
+    print(f'GPU Name: {deviceName}')
+except:
+    pass
+
+# Select device
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
+
+print(f'CUDA Available: {torch.cuda.is_available()}\n'
+      f'CUDA Version: {torch.version.cuda}\n'
+      f'Training Device: {device}')
