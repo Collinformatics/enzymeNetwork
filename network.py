8ead4c1ea33c44a52ed9ac254b2ca8fd53a843e0
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-09-26 16:14:06 -0500

    Add files via upload

--- network.py
+++ network.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import os
+import pandas as pd
 import subprocess
 
 
@@ -10,7 +11,7 @@ inNodeColor = 'green'
 inNodeSize = 700
 inFontSize = 12
 inLineWidth = 1.5
-inArrowSize = 20
+inArrowSize = 18
 inArc = 0.3 # Arrow curve
 
 # Input: Save Params
@@ -31,6 +32,37 @@ data = {
 
 
 # ========================================================================================
+def plotNetwork(network):
+    """
+    :param network: A dictionary containing the names of each node as keys, and a list of
+        nodes a given node makes a connection to.
+    """
+    print('==================================== Network '
+          '====================================')
+    df = pd.DataFrame(0, index=network.keys(), columns=network.keys())
+    for node in df.columns:
+        for connNode in network[node]:
+            df.loc[connNode, node] = 1
+    print(f'{df}\n')
+
+    # Plot network
+    G = nx.from_dict_of_lists(network, create_using=nx.DiGraph())
+    nx.draw(
+        G, with_labels=True, node_size=inNodeSize, node_color=inNodeColor,
+        edge_color='black', min_source_margin=15, min_target_margin=15,
+        font_size=inFontSize, font_weight='bold',
+        width=inLineWidth, arrowsize=inArrowSize,  # arrow head size
+        arrowstyle='-|>',  # '->', '-|>', '-['
+        connectionstyle=f'arc3,rad={inArc}'  # curve for parallel edges
+    )
+
+    # Save figure
+    plt.savefig(savePath, bbox_inches='tight')
+    print(f'Graph was saved at path:\n\t{savePath}\n\n')
+    subprocess.run(['xdg-open', savePath])  # Open fig in web browser
+
+
+# ========================================================================================
 # Save params
 if not inFileName.endswith('.png') or inFileName.endswith('.jpeg'):
     inFileName += '.png'
@@ -40,15 +72,4 @@ if not os.path.exists(inSaveDir):
 
 
 # Graph network
-G = nx.from_dict_of_lists(data, create_using=nx.DiGraph())
-nx.draw(G, with_labels=True, node_size=inNodeSize, node_color=inNodeColor,
-        edge_color='black', font_size=inFontSize, font_weight='bold',
-        width=inLineWidth, arrowsize=inArrowSize, # arrow head size
-        arrowstyle='->', # '->', '-|>', '-['
-        connectionstyle=f'arc3,rad={inArc}' # curve for parallel edges
-)
-
-# Save figure
-plt.savefig(savePath, bbox_inches='tight')
-print(f'Graph was saved at path:\n\t{savePath}')
-subprocess.run(['xdg-open', savePath]) # Open fig in web browser
+plotNetwork(data)
