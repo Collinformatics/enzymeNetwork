cc1080e24389d2a4c807d4e3985e4d3196cdc5be
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-09-26 15:25:44 -0500

    Add files via upload

--- network.py
+++ network.py
@@ -3,9 +3,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 # ========================================================================================
+# Input: Figures
+inNodeColor = 'green'
+inNodeSize = 700
+inFontSize = 12
+inArrowSize = 25
+inArc = 0.3 # Arrow curve
+
+# Input: Save Params
 inFileName = 'net'
 inSaveDir = 'Networks'
 
+
+# ========================================================================================
+# Input: Data
 data = {
     'A': ['B', 'D'],
     'B': ['C'],
@@ -17,6 +28,7 @@ data = {
 
 
 # ========================================================================================
+# Save params
 if not inFileName.endswith('.png') or inFileName.endswith('.jpeg'):
     inFileName += '.png'
 savePath = os.path.join(inSaveDir, inFileName)
@@ -26,7 +38,14 @@ if not os.path.exists(inSaveDir):
 
 # Graph network
 G = nx.from_dict_of_lists(data, create_using=nx.DiGraph())
-nx.draw(G, with_labels=True, node_size=700, font_size=12, node_color='green')
+nx.draw(G, with_labels=True, node_size=inNodeSize, font_size=inFontSize,
+        node_color=inNodeColor,
+        arrowsize=inArrowSize, # arrow head size
+        arrowstyle='->', # '->', '-|>', '-['
+        connectionstyle=f'arc3,rad={inArc}' # curve for parallel edges
+)
+
+# Save figure
 plt.savefig(savePath, bbox_inches='tight')
 print(f'Graph was saved at path:\n\t{savePath}')
 os.system(f'xdg-open {savePath}')
